b89bbb78aa5064d44a6c0681435994100404ddd8
Harlon Garcia <harlondesigner@gmail.com>
2023-01-14 12:25:25 -0300

    feat: create banner on home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,16 @@
 import "./Home.scss";
 
 const Home = () => {
-  return <div className="home_container">Home works!</div>;
+  return (
+    <div className="home_container">
+      <div className="home_header">
+        <h1>Coding For Free</h1>
+        <small>
+          An open-source project to help people who are beginning a new company
+        </small>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
