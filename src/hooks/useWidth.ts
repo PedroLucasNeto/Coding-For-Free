719f57034dcd439a0269bffe8ef4852c418406b2
Harlon Garcia <harlondesigner@gmail.com>
2023-01-23 18:59:25 -0300

    feat: add useWidth hook

---
+++ src/hooks/useWidth.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export const useWidth = () => {
+  const [width, setWidth] = useState(0);
+
+  const handleResize = () => setWidth(window.innerWidth);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  console.log(width);
+
+  return width;
+};
