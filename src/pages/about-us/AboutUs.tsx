2580f09f57900d4fb94b42c6e636ebd2492902ee
PedroLucasNeto <pedrolukasneto@gmail.com>
2023-01-26 11:54:48 -0300

    feat: change in about us section

--- src/pages/about-us/AboutUs.tsx
+++ src/pages/about-us/AboutUs.tsx
@@ -1,23 +1,35 @@
 import "./AboutUs.scss";
+import Social from "../../components/social/Social";
 import gif from "../../assets/images/contributors_pictures/gifCoding.gif";
+
 function AboutUs() {
   return (
-    <section className="about_container">
-      <div className="about_box">
-        <div className="text_box">
-          <h1>About Us</h1>
-          <p>
-            We are Harlon and Pedro, two Information Systems' studens. Our
-            journey together started at the university and we both faced the
-            same problem how to get experience in software development. But as
-            good developers should do, we found a solution, lets offer free REAL
-            websites for people, this way we can develop our skills and acquire
-            experience.
-          </p>
+    <div>
+      <section className="about_container">
+        <div className="about_box">
+          <div className="text_box">
+            <h1>About Us</h1>
+            <p>
+              We are Harlon and Pedro, two Information Systems' studens. Our
+              journey together started at the university and we both faced the
+              same problem: How to get our first opportunitie in software
+              development without having experience? But as good developers
+              should do, we found a solution!
+            </p>
+            <p>
+              To solve this problem we created <mark>Coding For Free</mark>
+              which was a way help people build their online presence while
+              we're facing all the software development cycle's problems.
+            </p>
+          </div>
+
+          <img src={gif} alt="gif de pessoas codando" />
         </div>
-        <img src={gif} alt="gif de pessoas codando" />
-      </div>
-    </section>
+      </section>
+      <section>
+        <Social />
+      </section>
+    </div>
   );
 }
 
