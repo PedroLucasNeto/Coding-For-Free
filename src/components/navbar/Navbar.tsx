b24c4f431aa9f057177cacb8d790dd332f2844b7
PedroLucasNeto <pedrolukasneto@gmail.com>
2023-03-08 16:37:03 -0300

    feat: fix first section max-width / fix translation on navbar when mobile size

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import "./Navbar.scss";
 import { NavLink } from "react-router-dom";
 import TranslationButton from "../shared/translation-button/TranslationButton";
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
 
 interface NavbarProps {
   isOpened: boolean;
@@ -9,22 +10,23 @@ interface NavbarProps {
 }
 const Navbar = ({ isOpened, handleClick }: NavbarProps) => {
   const [isTranslated, setIsTranslated] = useState(false);
+  const { t } = useTranslation();
   return (
     <ul className={`navbar ${isOpened ? "opened" : ""}`}>
       <li>
         <link rel="stylesheet" href="" />
         <NavLink to="/" onClick={() => handleClick(!isOpened)}>
-          Home
+          {t("header-mobile-item1")}
         </NavLink>
       </li>
       <li>
         <NavLink to="/contact" onClick={() => handleClick(!isOpened)}>
-          Contact
+          {t("header-mobile-item2")}
         </NavLink>
       </li>
       <li>
         <NavLink to="/about" onClick={() => handleClick(!isOpened)}>
-          About Us
+          {t("header-mobile-item3")}
         </NavLink>
       </li>
       <li>
