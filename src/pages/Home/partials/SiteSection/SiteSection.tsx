c8de4d0c94572909b0ea7cf3db7366da2034c190
PedroLucasNeto <pedrolukasneto@gmail.com>
2024-05-04 19:12:20 -0300

    feat: v1

--- src/pages/Home/partials/SiteSection/SiteSection.tsx
+++ src/pages/Home/partials/SiteSection/SiteSection.tsx
@@ -7,8 +7,10 @@ import "swiper/css/effect-fade";
 import "swiper/css/pagination";
 import SitesDeveloped from "./partials/SitesDeveloped/SitesDeveloped";
 import { useTranslation } from "react-i18next";
-import { Carousel } from "../../../../components/Carousel/Carousel";
+// import { Carousel } from "../../../../components/Carousel/Carousel";
 import styles from "./styles.module.scss";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation, Pagination } from "swiper";
 
 const SiteSection = () => {
   const siteRef = useRef(null);
@@ -38,17 +40,49 @@ const SiteSection = () => {
     >
       <div className={styles.site_section}>
         <h2>{t("sites-section-title")}</h2>;
-        <Carousel
-          children={sites.map((site, index) => {
+        <Swiper
+       speed={1200}
+       pagination={true}
+       
+       breakpoints={{
+         1560: {
+           slidesPerView: 5,
+           pagination: false,
+         },
+         1024: {
+           slidesPerView: 3,
+           spaceBetween: 20,
+        
+         },
+         768: {
+           slidesPerView: 3,
+         },
+         0: {
+           slidesPerView: 1,
+           spaceBetween: 10,
+         },
+       }}
+       centerInsufficientSlides={true}
+       centeredSlides={true}
+       centeredSlidesBounds={true}
+       autoplay={{
+         delay: 4000,
+         disableOnInteraction: false,
+       }}
+       modules={[Autoplay, Pagination]}
+     >
+          {sites.map((site, index) => {
             const siteData = {
               ...site,
               name: t("sites-section-name-" + site.name),
               description: t("sites-section-description-" + site.name),
             };
 
-            return <SitesDeveloped key={index} site={siteData} />;
+            return <SwiperSlide key={index} > 
+              <SitesDeveloped key={index} site={siteData} />;
+            </SwiperSlide>
           })}
-        />
+        </Swiper>
       </div>
     </motion.section>
   );
